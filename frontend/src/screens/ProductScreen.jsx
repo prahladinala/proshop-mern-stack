4262c8f8e11db68d1233fcf7c1519dc8b08f7de1
Prahlad Inala <prahlad2018@gmail.com>
2023-10-13 16:39:33 +0530

    Product Detail Page From backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,19 @@
-import { Link, useParams } from "react-router-dom";
-import products from "../products";
+import { Link,useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from 'axios'
 import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
 import Rating from "../components/Rating";
 
 const ProductScreen = () => {
+    const [product,setProduct] = useState({});
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+  useEffect(()=>{
+    const fetchProduct = async()=>{
+        const {data} = await axios.get(`/api/products/${productId}`);
+        setProduct(data)
+    }
+    fetchProduct()
+  },[productId])
   console.log(product);
 
   return (
